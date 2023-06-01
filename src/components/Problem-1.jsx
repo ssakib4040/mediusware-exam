2e7e0890e91cc0bfe11485827bc1296070c4dcc9
Sadman Sakib <ssakib4040@gmail.com>
2023-06-01 07:10:55 +0600

    Problem 1 solved

---
+++ src/components/Problem-1.jsx
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+
+const Problem1 = () => {
+  const [show, setShow] = useState("all");
+  const [tasks, setTasks] = useState([]);
+
+  const handleClick = (val) => {
+    setShow(val);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const nameInput = e.target.elements.name.value;
+    const statusInput = e.target.elements.status.value;
+
+    const newTask = {
+      name: nameInput,
+      status: statusInput,
+    };
+
+    setTasks([...tasks, newTask]);
+    e.target.reset();
+  };
+
+  const filteredTasks = tasks.filter((task) => {
+    if (show === "active") {
+      return task.status === "Active";
+    } else if (show === "completed") {
+      return task.status === "Completed";
+    } else {
+      return true;
+    }
+  });
+
+  const sortedTasks = [...filteredTasks].sort((a, b) => {
+    if (a.status === "Active" && b.status !== "Active") {
+      return -1;
+    } else if (a.status !== "Active" && b.status === "Active") {
+      return 1;
+    } else if (a.status === "Completed" && b.status !== "Completed") {
+      return -1;
+    } else if (a.status !== "Completed" && b.status === "Completed") {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  // Render the component UI with the updated code
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
+        <div className="col-6 ">
+          <form
+            className="row gy-2 gx-3 align-items-center mb-4"
+            onSubmit={handleSubmit}
+          >
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                name="name"
+              />
+            </div>
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Status"
+                name="status"
+              />
+            </div>
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="col-8">
+          <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "all" && "active"}`}
+                type="button"
+                onClick={() => handleClick("all")}
+              >
+                All
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "active" && "active"}`}
+                type="button"
+                onClick={() => handleClick("active")}
+              >
+                Active
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "completed" && "active"}`}
+                type="button"
+                onClick={() => handleClick("completed")}
+              >
+                Completed
+              </button>
+            </li>
+          </ul>
+          <div className="tab-content"></div>
+          <table className="table table-striped ">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sortedTasks.map((task, index) => (
+                <tr key={index}>
+                  <td>{task.name}</td>
+                  <td>{task.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Problem1;
